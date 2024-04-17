99de857d8420fae919e8df7f8366d956c905c161
jaymin0903 <148968840+jaymin0903@users.noreply.github.com>
2024-04-17 11:46:22 +0530

    create project

---
+++ RichTextEditor/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditorComponent } from './editor/editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EditorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
